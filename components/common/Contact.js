5bee03c522a7e337c6e6ddd7f600d1a5ea567cfc
Nico G <nicolas.giusti@nextsourcia.com>
2021-07-01 15:26:33 +0200

    [fix] Auto fill in contact form

--- components/common/Contact.js
+++ components/common/Contact.js
@@ -2,18 +2,33 @@ import { useEffect, useRef, useState } from 'react';
 import useTranslation                  from 'next-translate/useTranslation';
 import Button                          from '@components/ui/Button';
 import { setContact }                  from '@lib/aquila-connector/contact';
+import { useUser }                     from '@lib/hooks';
 
-export default function Contact({ classdiv, classinput, 'button-title': value, mode = 'send' }) {
+export default function Contact({ classdiv, classinput, 'button-title': value, mode = 'send', firstnameid = 'First-Name', lastnameid = 'Last-Name', emailid = 'Email' }) {
     const divRef                    = useRef();
     const [message, setMessage]     = useState();
     const [isLoading, setIsLoading] = useState(false);
+    const user                      = useUser();
     const { t }                     = useTranslation();
 
     useEffect(() => {
         let form = divRef.current;
         do { form = form.parentNode; } while (form.tagName !== 'FORM' && form.tagName !== 'HTML');
         form.onsubmit = onSubmitForm;
-    }, []);
+
+        // Auto fill for first/last name and email
+        if (user) {
+            if (form.querySelector(`#${firstnameid}`)) {
+                form.querySelector(`#${firstnameid}`).value = user.firstname;
+            }
+            if (form.querySelector(`#${lastnameid}`)) {
+                form.querySelector(`#${lastnameid}`).value = user.lastname;
+            }
+            if (form.querySelector(`#${emailid}`)) {
+                form.querySelector(`#${emailid}`).value = user.email;
+            }
+        }
+    }, [user]);
 
     const onSubmitForm = async (e) => {
         e.preventDefault();

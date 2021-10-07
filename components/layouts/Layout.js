406788e593174ad9fcf9935075c6c9d9dddc2d44
Nico G <nicolas.giusti@nextsourcia.com>
2021-10-07 17:41:48 +0200

    [new] Adding custom Head from CMS block "head"

--- components/layouts/Layout.js
+++ components/layouts/Layout.js
@@ -1,12 +1,22 @@
-import Header        from '@components/layouts/Header';
-import Footer        from '@components/layouts/Footer';
-import CookiesBanner from '@components/common/CookiesBanner';
-import BlockCMS      from '@components/common/BlockCMS';
-import Languages     from '@components/common/Languages';
+import Head             from 'next/head';
+import parse            from 'html-react-parser';
+import Header           from '@components/layouts/Header';
+import Footer           from '@components/layouts/Footer';
+import CookiesBanner    from '@components/common/CookiesBanner';
+import BlockCMS         from '@components/common/BlockCMS';
+import Languages        from '@components/common/Languages';
+import { useCmsBlocks } from '@lib/hooks';
 
 export default function Layout({ children }) {
+    const cmsBlocks    = useCmsBlocks();
+    const cmsBlockHead = cmsBlocks.find((b) => b.code === 'head'); // Getting CMS block "Head"
+
     return (
         <>
+            <Head>
+                {cmsBlockHead ? parse(cmsBlockHead.content) : null}
+            </Head>
+
             <BlockCMS nsCode="top-banner" />
 
             <Languages />

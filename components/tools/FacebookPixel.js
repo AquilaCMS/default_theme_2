6933300db601463ab374bb761515c78984008a24
Nico G <nicolas.giusti@nextsourcia.com>
2021-12-06 16:52:14 +0100

    [fix] Params URL for FB pixel

--- components/tools/FacebookPixel.js
+++ components/tools/FacebookPixel.js
@@ -3,26 +3,25 @@ import { useRouter } from 'next/router';
 import cookie        from 'cookie';
 import * as fbq      from '@lib/common/fb-pixel/fpixel';
 
-const handleRouteChange = () => {
-    fbq.pageview();
-};
-
 const FacebookPixel = ({ children }) => {
     const router = useRouter();
 
     useEffect(() => {
+        const handleRouteChange = (url) => {
+            fbq.pageview(url);
+        };
+
         const cookieNotice = cookie.parse(document.cookie).cookie_notice;
-        if (cookieNotice === true) {
+        if (cookieNotice === 'true') {
             // This pageview only trigger first time (it is important for Pixel to have real information)
-            fbq.pageview();
-        
+            fbq.pageview(router.asPath);
             router.events.on('routeChangeComplete', handleRouteChange);
         }
         
         return () => {
             router.events.off('routeChangeComplete', handleRouteChange);
         };
-    }, [router.events]);
+    }, [router.event]);
 
     return children;
 };

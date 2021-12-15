649f285f7a6631c9c3d0e526fb00b19902c72201
Nico G <nicolas.giusti@nextsourcia.com>
2021-12-15 11:18:37 +0100

    [fix] Update Facebook Pixel

--- components/tools/FacebookPixel.js
+++ components/tools/FacebookPixel.js
@@ -1,17 +1,18 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import cookie        from 'cookie';
-import * as fbq      from '@lib/common/fb-pixel/fpixel';
+import { useEffect }       from 'react';
+import Script              from 'next/script';
+import { useRouter }       from 'next/router';
+import * as fbq            from '@lib/common/fb-pixel/fpixel';
+import { useCookieNotice } from '@lib/hooks';
 
 const FacebookPixel = ({ children }) => {
-    const router = useRouter();
+    const { cookieNotice } = useCookieNotice();
+    const router           = useRouter();
 
     useEffect(() => {
         const handleRouteChange = (url) => {
             fbq.pageview(url);
         };
-
-        const cookieNotice = cookie.parse(document.cookie).cookie_notice;
+        
         if (cookieNotice === 'true') {
             // This pageview only trigger first time (it is important for Pixel to have real information)
             fbq.pageview(router.asPath);
@@ -23,7 +24,30 @@ const FacebookPixel = ({ children }) => {
         };
     }, [router.event]);
 
-    return children;
+    if (!fbq.FB_PIXEL_ID || cookieNotice !== 'true') return children;
+    return (
+        <>
+            {/* Global Site Code Pixel - Facebook Pixel */}
+            <Script
+                id="facebookpixel-script"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                    !function(f,b,e,v,n,t,s)
+                    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                    n.queue=[];t=b.createElement(e);t.async=!0;
+                    t.src=v;s=b.getElementsByTagName(e)[0];
+                    s.parentNode.insertBefore(t,s)}(window, document,'script',
+                    'https://connect.facebook.net/en_US/fbevents.js');
+                    fbq('init', ${fbq.FB_PIXEL_ID});
+                `,
+                }}
+            />
+            { children }
+        </>
+    );
 };
 
 export default FacebookPixel;

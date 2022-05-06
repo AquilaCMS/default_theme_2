b8e01464ba2c8eb40b741d321dae9ad242063905
devaquila <71753125+devaquila@users.noreply.github.com>
2022-05-06 10:12:19 +0200

    [Fix] module list must end by comma

--- modules/list_modules.js
+++ modules/list_modules.js
@@ -1,3 +1,3 @@
 export default [{jsx:require('./CartListItemsFoodOptions.js'),code:'aq-foodoptions',type:'cart-list-items'},
 {jsx:require('./AllergenFilter.js'),code:'aq-allergen-filter',type:'category-top-list'},
-{jsx:require('./AllergenList.js'),code:'aq-allergen-list',type:'product-tab'}]
+{jsx:require('./AllergenList.js'),code:'aq-allergen-list',type:'product-tab'},]

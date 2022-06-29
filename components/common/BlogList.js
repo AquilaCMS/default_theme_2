3dd246b5c4faaf7963a0ebb2ad354a8ab653c12c
Axel-Ange Finninger <finningeraxelange@gmail.com>
2022-06-29 17:23:24 +0200

    Nouveau système de blog multipage

--- components/common/BlogList.js
+++ components/common/BlogList.js
@@ -1,16 +1,18 @@
-
 import parse                from 'html-react-parser';
+import Link                 from 'next/link';
 import useTranslation       from 'next-translate/useTranslation';
 import { useComponentData } from '@lib/hooks';
 import { formatDate }       from '@lib/utils';
+
+
 
 export default function BlogList({ list = [] }) {
     const componentData = useComponentData();
     const { lang, t }   = useTranslation();
 
-    // Get data in redux store or prop list
+    // Get data in redux store or prop list 
     let blogList = componentData['nsBlogList'] || list;
-    
+    console.log(blogList);
     if (!blogList?.length) {
         return <div className="w-dyn-empty">
             <div>{t('components/blogList:noArticle')}</div>
@@ -19,23 +21,33 @@ export default function BlogList({ list = [] }) {
 
     return (
         <div className="content-section">
-            <div className="container">
-                {blogList.map((item) => (
-                    <div key={item._id} className="w-layout-grid content-grid" itemType="http://schema.org/Article">
-                        <div className="content-block">
-                            <h2 className="heading-5" style={{ marginBottom: '0px' }}>{item.title}</h2>
-                            <p style={{ fontStyle: 'italic' }}>{formatDate(item.createdAt, lang, { hour: '2-digit', minute: '2-digit', weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' })}</p>
-                            <div>{parse(item.content.text)}</div>
-                        </div>
-                        {
-                            item.img && 
-                            <div className="image-block">
-                                <img src={`/images/blog/578x266/${item._id}/${item.slug[lang]}${item.extension}`} alt={item.title} />
+            <div className="blog-container">
+                {blogList.map((article) => (
+                    <div key={article._id} className="blog-card" style={{ width: '26%', display: 'flex', flexDirection: 'column' }}>
+                        <img src={`/images/blog/578x266-80-crop-center/${article._id}/${article.slug[lang]}${article.extension}`} alt={article.title} className="blog-thumbnail" loading="lazy" />
+                        <div style={{ height: '100%' }}>
+                            <div className="blog-card-content">
+                                <div className="blog-title-wrap">
+                                    <h2 className="blog-title" style={{ marginBottom: '0px' }}>{article.title}</h2>
+                                </div>
+                                <p className="blog-date" style={{ fontStyle: 'italic' }}>{formatDate(article.createdAt, lang, { hour: '2-digit', minute: '2-digit', weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' })}</p>
+                                <p className="paragraph">{parse(article.content.resume.slice(0, 80))}[...]</p>
+                            
+                                <form className="w-commerce-commerceaddtocartform default-state">
+                                    <Link href={`/blog/${article.slug[lang]}`}><a><button type="submit" className="w-commerce-commerceaddtocartbutton order-button" style={{ alignItems: 'center' }}>Lire plus</button></a></Link>
+                                </form>
                             </div>
-                        }
+                        </div>
                     </div>
                 ))}
             </div>
+            {blogList.length > 2 &&
+            parse(`<div className="blog-see-more">
+                <button className="tab-link-round w-inline-block w-tab-link">Voir plus</button>
+            </div>`)
+            }
+
+            
         </div>
     );
 }
---
+++ pages/blog/[article].js
@@ -0,0 +1,97 @@
+import parse             from 'html-react-parser';
+import absoluteUrl       from 'next-absolute-url';
+import useTranslation    from 'next-translate/useTranslation';
+import Breadcrumb        from '@components/navigation/Breadcrumb';
+import Layout            from '@components/layouts/Layout';
+import BlockCMS          from '@components/common/BlockCMS';
+import { getBlogList }   from '@aquilacms/aquila-connector/api/blog';
+import { getBreadcrumb } from '@aquilacms/aquila-connector/api/breadcrumb';
+import { dispatcher }    from '@lib/redux/dispatcher';
+import { initAxios }     from '@lib/utils';
+import { formatDate }    from '@lib/utils';
+import getT              from 'next-translate/getT';
+import Link              from 'next/link';
+
+
+export async function getServerSideProps({ defaultLocale, locale, params, req, res, resolvedUrl }) {
+    initAxios(locale, req, res);
+
+    let blogArticle = {};
+    try {
+        const blogList = await getBlogList(locale);
+        
+        blogArticle = blogList.find(item => item.slug[locale] === params.article);
+    } catch (err) {
+        return { notFound: true };
+    }
+
+    let breadcrumb = [];
+    try {
+        breadcrumb = await getBreadcrumb(`${defaultLocale !== locale ? `/${locale}` : ''}${resolvedUrl}`);
+    } catch (err) {
+        const t = await getT(locale, 'common');
+        console.error(err.message || t('common:message.unknownError'));
+    }
+
+    // Get URLs for language change
+    // const urlsLanguages = [];
+    // if (staticPage) {
+    //     for (const [lang, sl] of Object.entries(staticPage.slug)) {
+    //         urlsLanguages.push({ lang, url: `/${sl}` });
+    //     }
+    // }
+
+    const actions = [
+        // {
+        //     type: 'PUSH_CMSBLOCKS',
+        //     func: getBlocksCMS.bind(this, ['bottom-parallax'], locale)
+        // }, {
+        //     type : 'SET_STATICPAGE',
+        //     value: staticPage
+        // }, {
+        //     type : 'SET_URLS_LANGUAGES',
+        //     value: urlsLanguages
+        // }
+    ];
+
+    const pageProps = await dispatcher(locale, req, res, actions);
+
+    // URL origin
+    const { origin }            = absoluteUrl(req);
+    pageProps.props.origin      = origin;
+    pageProps.props.blogArticle = blogArticle;
+    pageProps.props.breadcrumb  = breadcrumb;
+    return pageProps;
+}
+
+export default function BlogArticle({ blogArticle, breadcrumb, origin }) {
+    const { lang } = useTranslation();
+
+    return (
+        <Layout>
+            <div className="header-section-carte" style={{ backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.59), rgba(250, 140, 78, 0.72)), url('+`/images/blog/1920x266-100-crop-center/${blogArticle._id}/${blogArticle.slug[lang]}${blogArticle.extension}`+')' }}>
+                <div className="container-flex-2">
+                    <div className="title-wrap-centre">
+                        <h1 className="header-h1"><span className="brand-span">{blogArticle.title}</span></h1>
+                    </div>
+                </div>
+            </div>
+
+            <div className="content-section-short-product">
+                <Breadcrumb items={breadcrumb} origin={origin} />
+                <div className="container-flex-2">
+                    <div className="container-02">
+                        <h3>{blogArticle.title}</h3>
+                        <p className="blog-date" style={{ fontStyle: 'italic' }}>Le {formatDate(blogArticle.createdAt, lang, { hour: '2-digit', minute: '2-digit', weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' })}</p>
+                        <div   style={{ borderTop: '2px solid #ff8946', paddingTop: '15px', borderBottom: '2px solid #ff8946' }}>{parse(blogArticle.content.text)}</div>
+                        <Link href='/blog'><a><button type="button" className="button bottomspace w-button" style={{ marginTop: '60px' }}>Retour</button></a></Link>
+                    </div> 
+                    
+                </div>
+               
+            </div>
+
+            <BlockCMS nsCode="info-bottom-1" /> {/* TODO : il faudrait afficher le contenu d'une description de la catégorie rattachée ! */}
+        </Layout>
+    );
+}

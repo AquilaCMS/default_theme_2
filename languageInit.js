8266e44c2446867dd60552256023df4b5d0f4819
Nico G <nicolas.giusti@nextsourcia.com>
2023-02-02 16:29:25 +0100

    feat: copy default folder locales if new lang created

--- languageInit.js
+++ languageInit.js
@@ -1,14 +1,13 @@
-const fs                       = require('fs');
-const path                     = require('path');
-const { decodeBase64ToObject } = require('aql-utils');
+const path                         = require('path');
+const { fs, decodeBase64ToObject } = require('aql-utils');
 
-const setLanguage = (langs, defaultLanguage) => {
+const setLanguage = async (langs, defaultLanguage) => {
     const tabLangs  = langs.split(',');
     const themeName = path.basename(__dirname);
 
     global.aquila = decodeBase64ToObject(global.aquila);
 
-    const pathToTheme    = path.join(global.aquila.appRoot, 'themes', themeName, '/');
+    const pathToTheme    = path.join(global.aquila.appRoot, 'themes', themeName);
     const i18nSamplePath = path.join(pathToTheme, 'i18n.js.sample');
     const i18nFilePath   = path.join(pathToTheme, 'i18n.js');
 
@@ -19,12 +18,12 @@ const setLanguage = (langs, defaultLanguage) => {
     // Process to retrieve each module folder
     const anyPages        = json.pages['*'];
     const moduleThemePath = path.join(pathToTheme, 'modules');
-    if(fs.existsSync(moduleThemePath)) {
+    if (fs.existsSync(moduleThemePath)) {
         const moduleList = fs.readdirSync(path.join(pathToTheme, 'modules'), { withFileTypes: true })
             .filter((item) => item.isDirectory())
             .map((item) => item.name);
-        for (let i = 0; i < moduleList.length; i++) {
-            const modulePath = `modules/${moduleList[i]}`;
+        for (const m of moduleList) {
+            const modulePath = `modules/${m}`;
             anyPages.push(modulePath);
         }
     }
@@ -35,7 +34,54 @@ const setLanguage = (langs, defaultLanguage) => {
     res        = res.replace(/defaultLocale[\s\t]*:.*/, `defaultLocale: ${JSON.stringify(json.defaultLocale)},`);
     res        = res.replace(/pages[\s\t]*: {[^}]*/s, `pages: ${JSON.stringify(json.pages, null, 4).replace('}', '')}`);
     fs.writeFileSync(i18nFilePath, res);
+
+    // Create the folder for each language if it does not exist by copying that of the default language
+    await createTranslationFiles(tabLangs, defaultLanguage);
+
     console.log('Language initialization completed');
+};
+
+const createTranslationFiles = async (langs, defaultLanguage) => {
+    const themeName   = path.basename(__dirname);
+    const pathToTheme = path.join(global.aquila.appRoot, 'themes', themeName);
+    let pathFrom      = path.join(pathToTheme, 'locales');
+
+    // "locales" folder
+    await copyTranslationFiles(langs, defaultLanguage, pathFrom);
+
+    // "translations" folder for each module
+    const moduleThemePath = path.join(pathToTheme, 'modules');
+    if (fs.existsSync(moduleThemePath)) {
+        const moduleList = fs.readdirSync(path.join(pathToTheme, 'modules'), { withFileTypes: true })
+            .filter((item) => item.isDirectory())
+            .map((item) => item.name);
+        for (const m of moduleList) {
+            pathFrom = path.join(pathToTheme, 'modules', m, 'translations');
+            await copyTranslationFiles(langs, defaultLanguage, pathFrom);
+        }
+    }
+};
+
+const copyTranslationFiles = async (langs, defaultLanguage, pathFrom) => {
+    let fromLang = defaultLanguage;
+
+    let srcPathTranslation        = path.join(pathFrom, defaultLanguage);
+    const srcPathTranslationFiles = fs.existsSync(srcPathTranslation) ? fs.readdirSync(srcPathTranslation) : null;
+    if (!srcPathTranslationFiles || !srcPathTranslationFiles.length) {
+        const rootPathTranslation      = path.join(pathFrom);
+        const otherTranslationLangPath = fs.readdirSync(rootPathTranslation).find((lang) => lang !== defaultLanguage);
+        if (!otherTranslationLangPath) return;
+        srcPathTranslation = path.join(pathFrom, otherTranslationLangPath);
+        fromLang           = otherTranslationLangPath;
+    }
+
+    for (const lang of langs) {
+        const pathTranslationFilesDest = path.join(pathFrom, lang);
+        if (!fs.existsSync(pathTranslationFilesDest)) {
+            console.log(`Create folder for language [${lang}] from [${fromLang}] in [${pathFrom}]`);
+            await fs.copyRecursive(srcPathTranslation, pathTranslationFilesDest);
+        }
+    }
 };
 
 module.exports = {
